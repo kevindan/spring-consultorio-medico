d750121e5ff78ca56c36957251e97b67066c4e72
kevindan <fliper1960.2@gmail.com>
2018-05-19 11:33:22 -0500

    Commit inicial

---
+++ src/main/java/com/spring/consultorio/SpringConsultorioMedicoApplication.java
@@ -0,0 +1,12 @@
+package com.spring.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsultorioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsultorioMedicoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/consultorio/SpringConsultorioMedicoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.consultorio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConsultorioMedicoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
