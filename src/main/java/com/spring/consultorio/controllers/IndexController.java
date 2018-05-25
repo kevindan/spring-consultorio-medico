76589b6ca2b17dd5fc89d7657659903529f04c33
Kevin Daniel <fliper1960.2@gmail.com>
2018-05-25 18:35:55 -0500

    Creación de todas las plantillas de la aplicacion

--- src/main/java/com/spring/consultorio/controllers/IndexController.java
+++ src/main/java/com/spring/consultorio/controllers/IndexController.java
@@ -35,5 +35,54 @@ public class IndexController {
 	public String cita_medica() {
 		return "cita_medica";
 	}
-
+	
+	@GetMapping("/atender_cita_medica")
+	public String atender_cita_medica() {
+		return "atender_cita_medica";
+	}
+	
+	@GetMapping("/imagenologia")
+	public String imagenologia() {
+		return "imagenologia";
+	}
+	
+	@GetMapping("/consultorio")
+	public String consultorio() {
+		return "consultorio";
+	}
+	
+	@GetMapping("/medicamento")
+	public String medicamento() {
+		return "medicamento";
+	}
+	
+	@GetMapping("/diagnostico")
+	public String diagnostico() {
+		return "diagnostico";
+	}
+	
+	@GetMapping("/especialidad")
+	public String especialidad() {
+		return "especialidad";
+	}
+	
+	@GetMapping("/examen_laboratorio")
+	public String examen_laboratorio() {
+		return "examen_laboratorio";
+	}
+	
+	@GetMapping("/procedimiento")
+	public String procedimiento() {
+		return "procedimiento";
+	}
+	
+	@GetMapping("/establecimiento")
+	public String establecimiento() {
+		return "establecimiento";
+	}
+	
+	@GetMapping("/usuario")
+	public String usuario() {
+		return "usuario";
+	}
 }
