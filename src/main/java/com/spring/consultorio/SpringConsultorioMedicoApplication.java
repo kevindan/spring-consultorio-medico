1a472219b25b93037baaa68d492c7ea0f6750b8b
Kevin Daniel <fliper1960.2@gmail.com>
2018-06-05 18:22:37 -0500

    modelamiento en star uml

--- src/main/java/com/spring/consultorio/SpringConsultorioMedicoApplication.java
+++ src/main/java/com/spring/consultorio/SpringConsultorioMedicoApplication.java
@@ -2,11 +2,18 @@ package com.spring.consultorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringConsultorioMedicoApplication {
+public class SpringConsultorioMedicoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConsultorioMedicoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(SpringConsultorioMedicoApplication.class);
 	}
 }
