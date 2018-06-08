c1151b858d08b35d0246f68ac03c03554c6dfcd8
Kevin Daniel <fliper1960.2@gmail.com>
2018-06-08 18:30:14 -0500

    Modificando la clase Rol

--- src/main/java/com/spring/consultorio/entity/Rol.java
+++ src/main/java/com/spring/consultorio/entity/Rol.java
@@ -4,13 +4,79 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "roles")
 public class Rol implements Serializable {
 
+	@Id
+	@Column(name = "id_rol")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idRol;
+	// relacionar
 	private List<Usuario> usuarios;
+
+	@NotEmpty
+	@Column(name = "descripcion")
 	private String descripcion;
+
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	@Column(name = "fecha_registro")
 	private Date fechaRegistro;
+
+	@Column(name = "eliminado")
 	private int eliminado;
+
+	public Long getIdRol() {
+		return idRol;
+	}
+
+	public void setIdRol(Long idRol) {
+		this.idRol = idRol;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public int getEliminado() {
+		return eliminado;
+	}
+
+	public void setEliminado(int eliminado) {
+		this.eliminado = eliminado;
+	}
 
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/spring/consultorio/entity/TipoProducto.java
+++ src/main/java/com/spring/consultorio/entity/TipoProducto.java
@@ -2,11 +2,54 @@ package com.spring.consultorio.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tipo_productos")
 public class TipoProducto implements Serializable {
 
-	/**
-	 * 
-	 */
+	@Id
+	@Column(name = "id_tipo_producto")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTipoProducto;
+
+	@NotNull
+	@Column(name = "descripcion")
+	private String descripcion;
+
+	@Column(name = "eliminado")
+	private int eliminado;
+
+	public Long getIdTipoProducto() {
+		return idTipoProducto;
+	}
+
+	public void setIdTipoProducto(Long idTipoProducto) {
+		this.idTipoProducto = idTipoProducto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getEliminado() {
+		return eliminado;
+	}
+
+	public void setEliminado(int eliminado) {
+		this.eliminado = eliminado;
+	}
+
 	private static final long serialVersionUID = 1L;
 
 }
